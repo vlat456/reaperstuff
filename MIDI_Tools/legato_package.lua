3c1ed14a23b64554a5640be23c9e29a9e353acb6
Vladimir Karmishin <vlat456@gmail.com>
2025-12-14 11:47:14 +0100

    1

--- MIDI_Tools/legato_package.lua
+++ MIDI_Tools/legato_package.lua
@@ -1,8 +1,9 @@
+-- @metapackage
 -- @description Legato Tool - Creating legato effects and stuff. 
 -- @author drvlat
 -- @version 0.2.1
 -- @provides 
---      [nomain] modules/legato_common.lua
+--      modules/legato_common.lua
 --      [main=midi_editor,midi_inlineeditor,midi_eventlisteditor] Legato_Tool.lua
 --      [main=midi_editor,midi_inlineeditor,midi_eventlisteditor] Legato_Tool_Quick_Heal.lua 
 --      [main=midi_editor,midi_inlineeditor,midi_eventlisteditor] Legato_Tool_Quick_Legato.lua
