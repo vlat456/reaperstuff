932534a1dca690a9c96a608e2fe199fcd0e0e7f3
Vladimir Karmishin <vlat456@gmail.com>
2026-06-18 21:33:13 +0200

    fix: allow MIDI text events to write regardless of write_keyswitches check

--- Media_Tools/Media_Offset_Tool.lua
+++ Media_Tools/Media_Offset_Tool.lua
@@ -1,6 +1,6 @@
 -- @description Media Offset Tool
 -- @author drvlat
--- @version 1.7.3
+-- @version 1.7.4
 -- @about
 --   An ImGui-based utility for adjusting media offsets in REAPER.
 --   Supports three target modes selected via radio buttons:
@@ -26,7 +26,7 @@ package.path = reaper.ImGui_GetBuiltinPath() .. '/?.lua;' .. package.path
 local imgui = require('imgui')('0.9.3')
 
 -- Script variables
-local script_name = "Media Offset Tool v1.7.3"
+local script_name = "Media Offset Tool v1.7.4"
 local ctx = reaper.ImGui_CreateContext(script_name)
 local script_running = true
 local gui_state -- Forward declaration for helper functions
@@ -561,11 +561,9 @@ local function save_preset_name_to_targets(preset_name)
                     info.preset_name = ""
                 end
                 
-                -- Write preset name to MIDI Text Event (only when keyswitch writing is enabled)
-                if gui_state.write_keyswitches then
-                    local current_ppq = reaper.MIDI_GetPPQPosFromProjTime(take, info.start_time + (info.offset_ms / 1000.0))
-                    write_note_preset_text_event(take, current_ppq, preset_name)
-                end
+                -- Write preset name to MIDI Text Event
+                local current_ppq = reaper.MIDI_GetPPQPosFromProjTime(take, info.start_time + (info.offset_ms / 1000.0))
+                write_note_preset_text_event(take, current_ppq, preset_name)
             end
             save_take_note_presets(take, note_presets)
         end
@@ -1463,8 +1461,8 @@ local function apply_offset_to_targets(value, preset_name)
                 end
             end
 
-            -- 6. Write MIDI Text Events (safe to do, only when keyswitch writing is enabled)
-            if gui_state.write_keyswitches and preset_name then
+            -- 6. Write MIDI Text Events
+            if preset_name then
                 for i, info in ipairs(gui_state.selected_targets) do
                     local note_info = selected_notes[i]
                     if not note_info then break end
