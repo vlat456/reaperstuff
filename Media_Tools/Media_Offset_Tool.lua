8b508f2fcd2938c96220beb94a092d0f8f7cf706
Vladimir Karmishin <vlat456@gmail.com>
2026-06-19 18:14:58 +0200

    chore: bump v1.9.3
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)

--- Media_Tools/Media_Offset_Tool.lua
+++ Media_Tools/Media_Offset_Tool.lua
@@ -1,6 +1,6 @@
 -- @description Media Offset Tool
 -- @author drvlat
--- @version 1.9.2
+-- @version 1.9.3
 -- @about
 --   An ImGui-based utility for adjusting media offsets in REAPER.
 --   Supports three target modes selected via radio buttons:
